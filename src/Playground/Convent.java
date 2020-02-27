d2ee1621e1d69f0766369694f7bfd088b73e9d27
Sebastian <sebekk7@gmail.com>
2020-02-27 15:43:28 +0100

    usuniecie skanera

---
+++ src/Playground/Convent.java
@@ -0,0 +1,91 @@
+package Playground;
+
+import java.util.Scanner;
+
+public class Convent {
+
+    //    metrów na centymetry
+//    metrów na milimetry
+//    centymetrów na metry
+//    milimetrów na metry
+
+    Scanner scanner = new Scanner(System.in);
+
+    final int exit = 0;
+    final int conventerMeterOnKilometer = 1;
+    final int conventerKilometerOnMeter = 2;
+    final int kilometerOnMile = 3;
+    final int mileOnKilometer = 4;
+
+    void ControlLoop() {
+        int option;
+        do {
+            printOptions();
+            option = scanner.nextInt();
+            switch (option) {
+                case conventerMeterOnKilometer:
+                    conventerMeterOnKilometer();
+                    break;
+                case conventerKilometerOnMeter:
+                    conventerKilometerOnMeter();
+                    break;
+                case kilometerOnMile:
+                    kilometerOnMile();
+                    break;
+                case mileOnKilometer:
+                    mileOnKilometer();
+                    break;
+                case exit:
+                    exit();
+                    break;
+                default:
+                    System.out.println("Nie ma takiej opcji.");
+            }
+        } while (option != exit);
+    }
+
+    void conventerMeterOnKilometer() {
+        System.out.println("Ile metrow chcesz przeliczyc?");
+        double howMany = scanner.nextDouble();
+        double sumKilometer = howMany * 1000;
+        System.out.println(sumKilometer + "\n \n");
+    }
+
+    void conventerKilometerOnMeter() {
+        System.out.println("Ile kilometrow chcesz przeliczyc?");
+        double howMany = scanner.nextDouble();
+        double sumMeter = howMany / 1000;
+        System.out.println(sumMeter + "\n \n");
+    }
+
+    void kilometerOnMile() {
+        System.out.println("Ile kilometrow chcesz przeliczyc?");
+        double howMany = scanner.nextDouble();
+        double sumMeter = howMany * 1.6;
+        System.out.printf("%f kilometrow to %f mil. \n \n", howMany, sumMeter);
+    }
+
+    void mileOnKilometer() {
+        System.out.println("Ile mil chcesz przeliczyc?");
+        double howMany = scanner.nextDouble();
+        double sumMeter = howMany * 0.625;
+        System.out.printf("%f mil to %f kilometrow. \n \n", howMany, sumMeter);
+    }
+
+
+    void printOptions() {
+        System.out.println("Co chcesz zamienic?");
+        System.out.println(exit + " - wyjscie z programu");
+        System.out.println(conventerMeterOnKilometer + " - Metry na kilomerty");
+        System.out.println(conventerKilometerOnMeter + " - Kilometry na metry");
+        System.out.println(kilometerOnMile + " - Kilometry na mile");
+        System.out.println(mileOnKilometer + " - Mile na kilometry");
+    }
+
+    void exit() {
+        System.out.println("Koniec programu.");
+        // zamykamy strumień wejścia
+        scanner.close();
+    }
+
+}
