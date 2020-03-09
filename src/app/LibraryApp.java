ec087488d91bfa20286ef6559a21b14a409c1ba6
Sebastian <sebekk7@gmail.com>
2020-03-09 20:11:07 +0100

    Polimorfizm  - zmiana klasy Library i Publication

--- src/app/LibraryApp.java
+++ src/app/LibraryApp.java
@@ -3,6 +3,7 @@ package app;
  public class LibraryApp {
      private static final String APP_NAME = "Biblioteka v1.1";
 
+     //metoda main
     public static void main(String[] args) {
         System.out.println(APP_NAME);
         LibraryControl libControl = new LibraryControl();
