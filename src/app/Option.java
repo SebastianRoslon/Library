e3d2f1bdf27e7d999e2d5ab1e0df8ccd02f3860f
Sebastian <sebekk7@gmail.com>
2020-07-08 19:57:52 +0200

    obsluga wyjatkow

--- src/app/Option.java
+++ src/app/Option.java
@@ -1,19 +1,16 @@
 package app;
 
-public enum Option {
-    EXIT(0, "Wyjscie z programu"),
-    ADD_BOOKS(1, "Dodanie ksiazki"),
-    ADD_MAGAZINE(2, "Dodanie magazynu/gazety"),
-    PRINT_BOOKS(3,"Wyswietlenie dostepnych ksiazek"),
-    PRINT_MAGAZINES(4, "Wyswietlenie dostepnych magazynow/gazet");
+import app.exception.NoSuchOptionException;
+
+enum Option {
+    EXIT(0, "Wyjście z programu"),
+    ADD_BOOK(1, "Dodanie książki"),
+    ADD_MAGAZINE(2,"Dodanie magazynu/gazety"),
+    PRINT_BOOKS(3, "Wyświetlenie dostępnych książek"),
+    PRINT_MAGAZINES(4, "Wyświetlenie dostępnych magazynów/gazet");
 
     private int value;
     private String description;
-
-    Option(int value, String description) {
-        this.value = value;
-        this.description = description;
-    }
 
     public int getValue() {
         return value;
@@ -23,12 +20,21 @@ public enum Option {
         return description;
     }
 
+    Option(int value, String desc) {
+        this.value = value;
+        this.description = desc;
+    }
+
     @Override
     public String toString() {
         return value + " - " + description;
     }
 
-    static Option createFromInt(int option){
-        return Option.values()[option];
+    static Option createFromInt(int option) throws NoSuchOptionException {
+        try {
+            return Option.values()[option];
+        } catch(ArrayIndexOutOfBoundsException e) {
+            throw new NoSuchOptionException("Brak opcji o id " + option);
+        }
     }
 }
