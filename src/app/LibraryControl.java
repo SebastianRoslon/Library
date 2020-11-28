26e0a23a78d284564120b4712b44d8a4cc4fba71
Sebastian <sebekk7@gmail.com>
2020-11-28 16:27:44 +0100

    Biblioteka v2.3 - dodanie wyrazenia lambda

--- src/app/LibraryControl.java
+++ src/app/LibraryControl.java
@@ -6,8 +6,6 @@ import io.DataReader;
 import io.file.FileManager;
 import io.file.FileManagerBuilder;
 import model.*;
-import model.comparator.AlphabeticalTitleComparator;
-import java.util.Comparator;
 import java.util.InputMismatchException;
 
 class LibraryControl {
@@ -125,20 +123,20 @@ class LibraryControl {
     }
 
     private void printBooks() {
-        printer.printBooks(library.getSortedPublications(new AlphabeticalTitleComparator()));
+        printer.printBooks(library.getSortedPublications(
+                (p1, p2) -> p1.getTitle().compareToIgnoreCase(p2.getTitle())
+        ));
     }
 
     private void printMagazines() {
-        printer.printMagazines(library.getSortedPublications(new AlphabeticalTitleComparator()));
+        printer.printMagazines(library.getSortedPublications(
+                (p1, p2) -> p1.getTitle().compareToIgnoreCase(p2.getTitle())
+        ));
     }
 
     private void printUsers() {
-        printer.printUsers(library.getSortedUsers(new Comparator<LibraryUser>() {
-            @Override
-            public int compare(LibraryUser p1, LibraryUser p2) {
-                return p1.getLastName().compareToIgnoreCase(p2.getLastName());
-            }
-        }));
+        printer.printUsers(library.getSortedUsers(
+                (p1, p2) -> p1.getLastName().compareToIgnoreCase(p2.getLastName())));
     }
 
     private void deleteMagazine() {
